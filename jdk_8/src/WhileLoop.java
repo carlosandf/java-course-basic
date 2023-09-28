e3d9cb6f299a10c246915ef7079f553f9ec2f0b7
carlosandf <carlos.andres.ff2912@gmail.com>
2023-09-28 11:11:14 -0500

    Update While loop

--- jdk_8/src/WhileLoop.java
+++ jdk_8/src/WhileLoop.java
@@ -1,9 +1,21 @@
 public class WhileLoop {
+    static boolean isTurnOnLight = false;
     public static void main(String[] args) {
+        isTurnOnLight = turnOnOffLight();
 
+        int i = 1;
+        while (isTurnOnLight && (i <= 10)) {
+            System.out.println(i + " " + sosMessage());
+            i++;
+        }
     }
 
-    public static void printSOS () {
-        System.out.println(". . . _ _ _ . . .");
+    public static String sosMessage () {
+        return ". . . _ _ _ . . .";
+    }
+
+    public static boolean turnOnOffLight () {
+        // isTurnOnLight ? false : true; -> ternarias
+        return !isTurnOnLight;
     }
 }
